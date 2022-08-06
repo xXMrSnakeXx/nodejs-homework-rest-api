08a67e17e6400572ccdc5bc0d213b13af942af32
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2022-08-06 13:40:46 +0300

    ДЗ

---
+++ controllers/users/setAvatar.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+
+const { User } = require("../../models");
+
+const avatarsDir = path.join(__dirname, "../","../", "public", "avatars");
+
+const setAvatar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { path: tmpPath, originalname } = req.file;
+
+    const img = await Jimp.read(tmpPath);
+    await img.resize(250, 250);
+    await img.writeAsync(tmpPath);
+
+    const [extension] = originalname.split(".").reverse();
+    const newName = `${_id}.${extension}`;
+    const uploadPath = path.join(avatarsDir, newName);
+    await fs.rename(tmpPath, uploadPath);
+    const avatarURL = path.join("avatars", newName);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({
+      avatarURL,
+    });
+  } catch (error) {
+    await fs.unlink(req.file.path);
+    throw error;
+  }
+};
+
+module.exports = setAvatar;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/users");
 
-const { auth } = require("../../middlewares");
+const { auth , upload} = require("../../middlewares");
 
 const { ctrlWrapper } = require("../../helpers");
 
@@ -17,5 +17,7 @@ router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
 router.patch("/", auth, ctrlWrapper(ctrl.updateSubscriptionUser));
+
+router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(ctrl.setAvatar));
 
 module.exports = router;
